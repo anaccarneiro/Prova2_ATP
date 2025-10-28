6221eb8ec2f24060cd0343709d3eb80aec1b910b
Ana Clara <acccarneiro@sga.pucminas.br>
2025-10-28 15:47:30 -0300

    Ex6 Finalizado

---
+++ Ex6/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Ex6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*6. Faça um programa que receba do usuário um vetor com 10 posições. Em seguida
+            deverá ser impresso o maior e o menor elemento do vetor.*/
+
+            int[] vetor = new int[10];
+
+            for (int i = 0; i < vetor.Length; i++)
+            {
+                Console.WriteLine($"Informe o {i + 1}º número:");
+                vetor[i] = int.Parse(Console.ReadLine());
+            }
+
+            int maiorPosicao = vetor[0];
+            int menorPosicao = vetor[0];
+
+            for (int i = 0; i < vetor.Length; i++)
+            {
+                if (vetor[i] > maiorPosicao)
+                {
+                    maiorPosicao = vetor[i];
+                }
+                else if (vetor[i] < menorPosicao)
+                {
+                    menorPosicao = vetor[i];
+                }
+            }
+            Console.WriteLine(maiorPosicao + " " + menorPosicao);
+        }
+    }
+}
