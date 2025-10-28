db819dd245d22067fb656598ccf48494af37a04b
Ana Clara <acccarneiro@sga.pucminas.br>
2025-10-28 15:28:16 -0300

    Ex8 Finalizado

---
+++ Ex8/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Ex8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*8. Crie um programa que lê 6 valores inteiros e, em seguida, mostre na tela os valores lidos na
+            ordem inversa.*/
+
+            int[] vetor = new int[6];
+
+            for (int i = 0; i < vetor.Length; i++)
+            {
+                Console.WriteLine($"Informe o {i + 1}º número:");
+                vetor[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = vetor.Length - 1; i >= 0 ; i--)
+            {
+                Console.Write(vetor[i]);
+            }
+        }
+    }
+}
