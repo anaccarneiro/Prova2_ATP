725a0e54d16ba401fb64800cb3bfb7327af2bbc6
Ana Clara <acccarneiro@sga.pucminas.br>
2025-10-29 14:46:17 -0300

    Ex8_Matrizes Finalizado

---
+++ Ex8_Matrizes/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Ex8_Matrizes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Leia uma matriz de 3 x 3 elementos. Calcule a soma dos elementos que estao acima da ˜
+            diagonal principal.*/
+
+            int[,] matriz = new int[3, 3];
+            Random num = new Random();
+            int soma = 0;
+
+            for (int linha = 0; linha < matriz.GetLength(0); linha++)
+            {
+                for (int coluna = 0; coluna < matriz.GetLength(1); coluna++)
+                {
+                    matriz[linha, coluna] = num.Next(10);
+                    Console.Write(matriz[linha, coluna] + "\t");
+                }
+                Console.WriteLine();
+            }
+
+            for (int linha = 0; linha < matriz.GetLength(0); linha++)
+            {
+                for (int coluna = matriz.GetLength(1) - 1; coluna > linha; coluna--)
+                {
+                    soma += matriz[linha, coluna];
+                }
+            }
+            Console.WriteLine(soma);
+        }
+    }
+}
